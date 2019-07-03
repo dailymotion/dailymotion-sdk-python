9e71385a6e1b7085f9ad8beb9074dc70222734de
Nicolas Ferron <nicolas.ferron@dailymotion.com>
2019-07-03 11:10:36 +0200

    DAILY-60532 Update classifiers

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 import os, sys
 
 setup(name='dailymotion',
-      version='0.2.2',
+      version='0.2.3',
       description='Dailymotion API SDK',
       long_description='Dailymotion API SDK',
-      download_url='https://github.com/dailymotion/dailymotion-sdk-python/archive/0.2.2.tar.gz',
       classifiers=[
         "Programming Language :: Python",
         "Development Status :: 5 - Production/Stable",
@@ -14,6 +13,7 @@ setup(name='dailymotion',
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
+        "Programming Language :: Python :: 3"
         ],
       keywords=['dailymotion', 'api', 'sdk', 'graph'],
       author='Samir AMZANI',
